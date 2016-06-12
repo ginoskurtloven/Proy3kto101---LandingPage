f35393835100a7f01f26bfee313b336d140f7970
ginoskurtloven <ginoskurtloven@gmail.com>
2016-06-12 16:54:56 +0800

    Started Working Setting Up The Pages

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Proy3kto101- Simple Landing Page</title>
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="css/bootstrap/css/bootstrap.css">
+	<!-- Bootstrap Theme -->
+	<link rel="stylesheet" href="css/bootstrap/css/bootstrap-theme.css">
+</head>
+<body>
+	<div style="padding-top: 50px;" class="fluid-container">
+		<div class="col-md-offset-1 col-md-4">
+			<form action="" method="POST">
+				<div class="form-group">
+					<label for="FullName">Full Name:</label>
+					<input class="form-control" type="text" name="FullName" value="" id="FullName">
+				</div>
+				<div class="form-group">
+					<label for="EmailAddress">Email Address:</label>
+					<input class="form-control" type="email" name="EmailAddress" id="EmailAddress">
+				</div>
+				<div class="form-group">
+					<label for="ContactNumber">Contact Number:</label>
+					<input class="form-control" type="number" min="1" max="15" name="ContactNumber" id="ContactNumber">
+				</div>
+				<div class="form-group">
+					<label for="Address">Address:</label>
+					<input class="form-control" type="text" name="Address" id="Address">
+				</div>
+			</form>
+		</div>
+		<div class="col-md-offset-6">
+			<h2>LOREM IPSUM DOLOR</h2>
+			<h3>SOME SUB-HEADING HERE!</h3>
+		</div>
+	</div> <!-- end of container -->
+</body>
+</html>

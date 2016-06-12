35ddd18afd166d18cb4ed56b3f055fce2cf0f9e2
ginoskurtloven <ginoskurtloven@gmail.com>
2016-06-12 18:18:14 +0800

    Created Initial Page and the form

--- index.php
+++ index.php
@@ -10,33 +10,81 @@
 	<link rel="stylesheet" href="css/bootstrap/css/bootstrap.css">
 	<!-- Bootstrap Theme -->
 	<link rel="stylesheet" href="css/bootstrap/css/bootstrap-theme.css">
+	<!-- Style.css -->
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<div style="padding-top: 50px;" class="fluid-container">
+	<div style="padding-top: 60px;" class="fluid-container">
+	<div class="row">
 		<div class="col-md-offset-1 col-md-4">
+		<div class="form-container">
+		<h1>REQUEST DEMO</h1>
 			<form action="" method="POST">
 				<div class="form-group">
-					<label for="FullName">Full Name:</label>
-					<input class="form-control" type="text" name="FullName" value="" id="FullName">
+					<input class="form-control input-lg" type="text" name="FirstName" value="" id="FirstName" placeholder="First Name (Required)">
 				</div>
 				<div class="form-group">
-					<label for="EmailAddress">Email Address:</label>
-					<input class="form-control" type="email" name="EmailAddress" id="EmailAddress">
+					<input class="form-control input-lg" type="text" name="LastName" id="LastName" placeholder="Last Name (Required)">
 				</div>
 				<div class="form-group">
-					<label for="ContactNumber">Contact Number:</label>
-					<input class="form-control" type="number" min="1" max="15" name="ContactNumber" id="ContactNumber">
+					<input class="form-control input-lg" type="email" name="EmailAddress" id="EmailAddress" placeholder="Email Address (Required)">
 				</div>
 				<div class="form-group">
-					<label for="Address">Address:</label>
-					<input class="form-control" type="text" name="Address" id="Address">
+					<input class="form-control input-lg" type="number" min="1" max="32" name="PhoneNumber" id="PhoneNumber" placeholder="Phone Number (Required)">
 				</div>
+				<div class="form-group">
+					<input class="form-control input-lg" type="text" name="CompanyName" id="CompanyName" placeholder="Company Name (Required)">
+				</div>
+				<div class="form-group">
+					<input class="form-control input-lg" type="text" name="JobTitle" id="JobTitle" placeholder="Job Titl (Required)">
+				</div>
+				<div class="form-group">
+					<select class="form-control input-lg" name="" id="">
+						<option value="" disabled="disabled" selected="selected">Company Size (Required)</option>
+						<option value="">Personal</option>
+						<option value="">50 Employees or Less</option>
+						<option value="">51 to 250 Employees</option>
+						<option value="">251 to 750 Employees</option>
+						<option value="">751 Employees or More</option>
+						<option value="">Agency</option>
+					</select>
+				</div>
+				<div class="form-group">
+					<select name="" id="" class="form-control input-lg">
+						<option value="" disabled="disabled" selected="selected">Industry (Required)</option>
+						<option value="Agency">Agency</option>
+						<option value="Automotive">Automotive</option>
+						<option value="Consumer Packaged Goods">Consumer Packaged Goods</option>
+						<option value="E-Commerce">E-Commerce</option>
+						<option value="Education">Education</option>
+						<option value="Entertainment">Entertainment</option>
+						<option value="Financial Services">Financial Services</option>
+						<option value="Food and Beverage">Food and Beverage</option>
+						<option value="Government Organization">Government Organization</option>
+						<option value="Healthcare">Healthcare</option>
+						<option value="Non-Profit">Non-Profit</option>
+						<option value="Publishing">Publishing</option>
+						<option value="Retail">Retail</option>
+						<option value="Technology">Technology</option>
+						<option value="Telecommunications">Telecommunications</option>
+						<option value="Travel - Hospitality">Travel - Hospitality</option>
+					</select>
+				</div>
+				<input type="submit" class="btn btn-success btn-block btn-lg">
 			</form>
 		</div>
-		<div class="col-md-offset-6">
-			<h2>LOREM IPSUM DOLOR</h2>
-			<h3>SOME SUB-HEADING HERE!</h3>
 		</div>
+		<div class="col-md-offset-1 col-md-4">
+			<h2>GET FREE DOMAIN NAME & HOSTING</h2>
+			<h3>SUB-HEADING HERE!</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In voluptates, nesciunt aliquam nobis doloremque ipsam ducimus, velit, vero, nam voluptate repellendus sed numquam ullam impedit omnis quasi at magnam dicta.</p>
+			&nbsp;
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore perspiciatis error nihil, ut quia ea eius dolor beatae cumque! Optio labore neque temporibus, quidem officia repellendus eos. Earum, magnam! Voluptatibus.</p>
+			&nbsp;
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore perspiciatis error nihil, ut quia ea eius dolor beatae cumque! Optio labore neque temporibus, quidem officia repellendus eos. Earum, magnam! Voluptatibus.</p>
+
+		</div>
+	</div>	<!-- end of row -->
 	</div> <!-- end of container -->
 </body>
 </html>
